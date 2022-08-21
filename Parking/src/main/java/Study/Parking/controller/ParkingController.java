137952733851fc76919b0b0968ef89af82eb0d2b
= <=>
2022-08-20 21:49:42 -0300

    first commit

---
+++ Parking/src/main/java/Study/Parking/controller/ParkingController.java
@@ -0,0 +1,12 @@
+package Study.Parking.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/parking")
+@Api(tags = "Parking Controller")
+public class ParkingController {
+
+}
---
+++ Parking/src/main/java/Study/Parking/repository/ParkingRepository.java
@@ -0,0 +1,10 @@
+package Study.Parking.repository;
+
+import Study.Parking.model.Parking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParkingRepository extends JpaRepository<Parking, String> {
+
+}
